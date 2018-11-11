aed3c589411ae9cea084754bd24676b7d6e8ead6
akash <akashu.hulihalli@gmail.com>
2018-11-11 20:51:47 +0100

    Adding UserInput method

--- src/assignment1/IntegersPart1.java
+++ src/assignment1/IntegersPart1.java
@@ -1,5 +1,20 @@
 package assignment1;
+import java.util.Scanner;
+
 
 public class IntegersPart1 {
-
+	public static int[] UserInput()
+	{
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Enter number of integers between 1 to 100:");
+		int n = scan.nextInt();
+		int[] ListOfNumbers = new int[n];
+		
+		System.out.println("Enter " + "numbers");
+		
+		for (int i=0;i<n;i++) {
+			ListOfNumbers[i] = scan.nextInt();
+		}
+		return ListOfNumbers;
+	}
 }
