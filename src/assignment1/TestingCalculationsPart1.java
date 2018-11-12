7295258de05663808bcbe580ed757102eaa6cd19
akash <akashu.hulihalli@gmail.com>
2018-11-13 00:12:51 +0100

    change in datatype

--- src/assignment1/TestingCalculationsPart1.java
+++ src/assignment1/TestingCalculationsPart1.java
@@ -16,14 +16,14 @@ class TestingCalculationsPart1 {
 		double function2 = test.FunctionRootn(7);
 		assertEquals(2.64,function2,0.1);
 		
-		int function3 = test.Function10Powern(4);
+		double function3 = test.Function10Powern(4);
 		assertEquals(10000,function3);
 		
 		int function4 = test.FunctionnCube(5);
 		assertEquals(125,function4);
 		
-		int function5 = test.Function2Powern(9);
-		assertEquals(512,function5);
+		double function5 = test.Function2Powern(5);
+		assertEquals(1.14,function5,0.1);
 		
 		double function6 = test.FunctionePowern(5);
 		assertEquals(148.4,function6,0.1);
