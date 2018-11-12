44803a4e80b9408bff2b2f372bc3b40a3a836ad9
Jasmine8596 <jasmine.padhye.0805@gmail.com>
2018-11-12 23:25:26 +0100

    percentage increase

--- src/assignment1/PieIsNotPie.java
+++ src/assignment1/PieIsNotPie.java
@@ -1,16 +1,36 @@
 package assignment1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class PieIsNotPie {
 	
-	public static double CircumferenceOfCircle(double diameter,int precision) {
+	public static double[] CircumferenceOfCircle(double diameter,int precision) {
+		double[] Areas = new double[precision+1];
 		
-		Double pi = BigDecimal.valueOf(Math.PI).setScale(precision, RoundingMode.HALF_UP).doubleValue();
+		Areas[0] = 3*diameter;
+		
+		for(int i=1; i <= precision ; i++) {
+			Double pi = BigDecimal.valueOf(Math.PI).setScale(i, RoundingMode.HALF_UP).doubleValue();
+			Areas[i] = pi*diameter;
+		}
 
-		return pi*diameter;
+		return Areas;
+	}
+	
+	public static double[] PercentageIncrease(double[] Areas) {
+		double[] PercentageIncrease = new double[Areas.length];
+		
+		PercentageIncrease[0] = 0;
+		
+		for(int i = 1; i < Areas.length ; i++) {
+			double increase = Areas[i] - Areas[i-1];
+			PercentageIncrease[i] = increase * 100 / Areas[i-1];
+		}
+		
+		return PercentageIncrease;
 	}
 	
 	public static void main(String[] args) {
@@ -23,7 +43,12 @@ public class PieIsNotPie {
 		int precision = scan.nextInt();
 		scan.close();
 		
+		double[] circumference = CircumferenceOfCircle(diameter,precision);
+		
 		System.out.println("Circumference of the circle is:");
-		System.out.println(CircumferenceOfCircle(diameter,precision));
+		System.out.println(Arrays.toString(circumference));
+		
+		System.out.println("Percentage Increase:");
+		System.out.println(Arrays.toString(PercentageIncrease(circumference)));
 	}
 }
