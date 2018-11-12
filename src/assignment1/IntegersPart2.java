e520c20e58f508788122b49e513dc6ebaa26e0d3
akash <akashu.hulihalli@gmail.com>
2018-11-12 21:44:50 +0100

    integers part 2 code

--- src/assignment1/IntegersPart2.java
+++ src/assignment1/IntegersPart2.java
@@ -1,5 +1,30 @@
 package assignment1;
 
-public class IntegersPart2 {
+import java.util.*;
 
+public class IntegersPart2 {
+	
+	public static ArrayList<Integer> GetPrimeNumbers(int LargestNumber) {
+		ArrayList<Integer> primenumbers = new ArrayList<Integer>(LargestNumber);
+		boolean flag = false;
+		for(int num = 1; num <= LargestNumber; num++) {
+			for(int i=2; i <= num/2; i++) {
+				if(num % i == 0) {
+					flag = true;
+					break;
+				}
+				else {
+					flag = false;
+				}
+			}
+			if(flag==false) {
+				primenumbers.add(num);
+			}
+		}
+		return primenumbers;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(GetPrimeNumbers(Integer.MAX_VALUE));
+	}
 }
