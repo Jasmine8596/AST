bd5a7b1b08d63aab1e7753f572c3e9d040fd5ae6
akash <akashu.hulihalli@gmail.com>
2018-11-12 14:52:48 +0100

    made required changes.

--- src/assignment1/IntegersPart1.java
+++ src/assignment1/IntegersPart1.java
@@ -3,40 +3,34 @@ import java.util.Scanner;
 
 
 public class IntegersPart1 {
-	public static int[] UserInput()
-	{
-		Scanner scan = new Scanner (System.in);
-		System.out.println("Enter number of integers between 1 to 100:");
-		int n = scan.nextInt();
-		int[] ListOfNumbers = new int[n];
-		
-		System.out.println("Enter " + "numbers");
-		
-		for (int i=0;i<n;i++) {
-			ListOfNumbers[i] = scan.nextInt();
-		}
-		
-		SumOfNumbers(ListOfNumbers);
-		ProductOfNumbers(ListOfNumbers);
-		return ListOfNumbers;
+  public static int LengthOfList;
+  public static int[] ListOfNumbers;
+
+	public static int UserInput(int LengthOfList){
+
+		return LengthOfList;
 	}
-	
-	public static int SumOfNumbers(int[] ListOfNumbers) {
+
+  public static int[] ListOfNumbers(int[] ListOfNumbers){
+
+    return ListOfNumbers;
+  }
+
+	public static int SumOfNumbers() {
 		int sum = 0;
-		
+
 		for(int i=0;i<ListOfNumbers.length;i++) {
 			sum = sum + ListOfNumbers[i];
 		}
 		return sum;
 	}
-	
-	public static int ProductOfNumbers(int[] ListOfNumbers) {
+
+	public static int ProductOfNumbers() {
 		int product = 1;
-		
+
 		for (int i=0; i<ListOfNumbers.length; i++) {
 			product = product * ListOfNumbers[i];
 		}
 		return product;
-		
 	}
 }
