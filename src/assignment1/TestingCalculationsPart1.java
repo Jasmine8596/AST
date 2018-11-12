296d6a8697f7bc0505f6e9c74cabae62cc7480a8
akash <akashu.hulihalli@gmail.com>
2018-11-12 23:55:25 +0100

    calculations part 1 test code

--- src/assignment1/TestingCalculationsPart1.java
+++ src/assignment1/TestingCalculationsPart1.java
@@ -8,7 +8,25 @@ class TestingCalculationsPart1 {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		CalculationsPart1 test = new CalculationsPart1();
+		
+		int function1 = test.Function2n(3);
+		assertEquals(6,function1);
+		
+		double function2 = test.FunctionRootn(7);
+		assertEquals(2.64,function2,0.1);
+		
+		int function3 = test.Function10Powern(4);
+		assertEquals(10000,function3);
+		
+		int function4 = test.FunctionnCube(5);
+		assertEquals(125,function4);
+		
+		int function5 = test.Function2Powern(9);
+		assertEquals(512,function5);
+		
+		double function6 = test.FunctionePowern(5);
+		assertEquals(148.4,function6,0.1);
 	}
 
 }
