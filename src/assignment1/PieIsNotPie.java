68976c0d0d63a8ac1eb5e3e310b09b6ff4f412df
Jasmine8596 <jasmine.padhye.0805@gmail.com>
2018-11-12 22:47:01 +0100

    circumference of circle method

--- src/assignment1/PieIsNotPie.java
+++ src/assignment1/PieIsNotPie.java
@@ -1,5 +1,21 @@
 package assignment1;
 
-public class PieIsNotPie {
+import java.util.Scanner;
 
+public class PieIsNotPie {
+	
+	public static double CircumferenceOfCircle(double diameter) {
+		return Math.PI*diameter;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter diameter of circle:");
+		double diameter = scan.nextInt();
+		scan.close();
+		
+		System.out.println("Circumference of the circle is:");
+		System.out.println(CircumferenceOfCircle(diameter));
+	}
 }
