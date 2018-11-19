e510f75a5ef880a8db93607d11f72d006a3521ff
Jasmine8596 <jasmine.padhye.0805@gmail.com>
2018-11-19 23:04:42 +0100

    code for generating random floating numbers

--- AST-2016-WT-ExSheet-2-Padhye-Hulihalli/Ex6/DoublesPart1.java
+++ AST-2016-WT-ExSheet-2-Padhye-Hulihalli/Ex6/DoublesPart1.java
@@ -1,6 +1,8 @@
 package Ex6;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class DoublesPart1 {
 	public static int[] InputIntegers(int num1,int num2,int num3) {
@@ -8,7 +10,20 @@ public class DoublesPart1 {
 		return input;
 	}
 	
+	public static List<Double> GenerateRandomFloatingPoint(int MaxNumber){
+		List<Double> RandomNumbers =  new ArrayList<Double>();
+		
+		for(int i=0; i<MaxNumber; i++) {
+			RandomNumbers.add(Math.random());
+		}
+		return RandomNumbers;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(Arrays.toString(InputIntegers(1000,54,99)));
+		List<Double> randomnumbers = GenerateRandomFloatingPoint(10);
+		System.out.println("Random floating numbers:");
+		System.out.println(randomnumbers);
 	}
+	
 }
