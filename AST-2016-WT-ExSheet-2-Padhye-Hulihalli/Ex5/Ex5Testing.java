bfa952b7ad08589ea9bc0b65d22d876ab3b966a8
Jasmine8596 <jasmine.padhye.0805@gmail.com>
2018-11-20 00:31:29 +0100

    non-testable codes in Ex5

--- AST-2016-WT-ExSheet-2-Padhye-Hulihalli/Ex5/Ex5Testing.java
+++ AST-2016-WT-ExSheet-2-Padhye-Hulihalli/Ex5/Ex5Testing.java
@@ -12,6 +12,11 @@ class Ex5Testing {
 		
 		int[] expected = new int[] {2,3,5,7};
 		assertEquals(expected.length,test.FindingPrimeIntegers(10).size());
+		
+		/*The time required for execution of a program cannot be calculated manually
+		 * So, it is not possible to write test cases for the same.
+		 * But we can check the time with the help of in-built methods like System.currentTimeMillis()
+		 */
 	}
 
 }
