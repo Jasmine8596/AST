aeac6fbb8f9d93d51799829f8187a4d3e566dc2a
21akashuh <43093646+21akashuh@users.noreply.github.com>
2018-11-20 00:11:19 +0100

    test cases for doubles part 1

--- AST-2016-WT-ExSheet-2-Padhye-Hulihalli/Ex6/Wx6Testing.java
+++ AST-2016-WT-ExSheet-2-Padhye-Hulihalli/Ex6/Wx6Testing.java
@@ -2,6 +2,9 @@ package Ex6;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class Wx6Testing {
@@ -12,7 +15,23 @@ class Wx6Testing {
 		
 		int[] expected = new int[] {1000,54,99};
 		int[] output = test.InputIntegers(1000,54,99);
-		assertEquals(expected,output);
+		assertEquals(expected.length,output.length);
+		
+		List<Double> inputlist = new ArrayList<Double>();
+		inputlist.add(1.1);
+		inputlist.add(10.2);
+		inputlist.add(20.3);
+		double outputsum = test.SumOfNumbers(inputlist);
+		assertEquals(31.6,outputsum,0.1);
+		
+		double outputproduct = test.ProductOfNumbers(inputlist);
+		assertEquals(227.76,outputproduct,0.1);
+		
+		double outputaverage = test.AverageOfNumbers(inputlist);
+		assertEquals(10.5,outputaverage,0.1);
+		
+		double outputvariance = test.VarianceOfNumbers(inputlist);
+		assertEquals(61.49,outputvariance,0.1); 
 	}
 
 }
